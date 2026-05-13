fb65b6f307f5f33c172bbf2cfddb952f83b24813
Alejandro Maya Ureba <alejandromayaureba@gmail.com>
2026-05-13 19:46:31 +0200

    prueba para panel admin

--- backend/entrenamientos/admin.py
+++ backend/entrenamientos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Rutina, Ejercicio, EjercicioRutina
 
 # Register your models here.
+admin.site.register(Rutina)
+admin.site.register(Ejercicio)
+admin.site.register(EjercicioRutina)
