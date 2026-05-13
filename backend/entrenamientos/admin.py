5a8611a20363f0eed8d89dcbe1edb131e802364a
Alejandro Maya Ureba <alejandromayaureba@gmail.com>
2026-05-13 19:57:06 +0200

    .

--- backend/entrenamientos/admin.py
+++ backend/entrenamientos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Rutina, Ejercicio, EjercicioRutina
+from .models import Ejercicios, RutinasEjercicios, Rutinas
 
 # Register your models here.
-admin.site.register(Rutina)
-admin.site.register(Ejercicio)
-admin.site.register(EjercicioRutina)
+admin.site.register(Rutinas)
+admin.site.register(Ejercicios)
+admin.site.register(RutinasEjercicios)
