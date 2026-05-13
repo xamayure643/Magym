3caf31c1dc1eea20d047909a124b1dbbb0630c45
Alejandro Maya Ureba <alejandromayaureba@gmail.com>
2026-05-13 15:08:43 +0200

    Cambios para desplegar

--- backend/magym_core/settings.py
+++ backend/magym_core/settings.py
@@ -163,6 +163,7 @@ STATIC_URL = 'static/'
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
+    "http://alejandro.integramarketingdigital.es",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['alejandro.integramarketingdigital.es']
+  }
 })
