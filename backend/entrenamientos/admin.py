8c8fd5b9fbd61d50341320311bd8138525480a21
Alejandro Maya Ureba <alejandromayaureba@gmail.com>
2026-05-13 20:05:26 +0200

    Tablas para panel administración

--- backend/entrenamientos/admin.py
+++ backend/entrenamientos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ejercicios, RutinasEjercicios, Rutinas
+from .models import Ejercicios, RutinasEjercicios, Rutinas, UsuariosEjerciciosFavoritos
 
 # Register your models here.
 admin.site.register(Rutinas)
 admin.site.register(Ejercicios)
 admin.site.register(RutinasEjercicios)
+admin.site.register(UsuariosEjerciciosFavoritos)
--- backend/nutricion/admin.py
+++ backend/nutricion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alimentos, RegistrosNutricion
 
 # Register your models here.
+admin.site.register(Alimentos)
+admin.site.register(RegistrosNutricion)
