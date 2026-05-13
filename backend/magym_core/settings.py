12c3dc318821355022366784ce6536378549cfa3
Alejandro Maya Ureba <alejandromayaureba@gmail.com>
2026-05-13 19:37:43 +0200

    cambio settings

--- backend/magym_core/settings.py
+++ backend/magym_core/settings.py
@@ -159,7 +159,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
